484524b09ded4a847e15b24f58bfdf9e7aebaba4
gio <kge91186@mail.ru>
2024-09-22 00:06:11 +0300

    testing env working

--- my_module.py
+++ my_module.py
@@ -9,6 +9,15 @@ def summing(a,b):
     
     return c
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+val = os.environ.get('password')
+
+print(val)
+
 
 if __name__=='__main__':
     visual()
